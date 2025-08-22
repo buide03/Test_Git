c5b9ba717ee09d7883b9b20f5303a987c9be63d7
buide03 <buidinhde2003tkk@gmail.com>
2025-08-22 15:34:43 +0700

    fix conffilict

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
 #include<iostream>
 int main(){
     std :: string userName;
-    int userAge;
     std::cout<<"Nhap ho ten";
     std::getline(std::cin,userName);
-
     std::cin>>userAge;
-
     std::cout<<"userName:"<<userName<<std::endl;
 
 }
